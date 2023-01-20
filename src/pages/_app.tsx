d08d085e295e9acc478ac4f8f0b5f6f546badf50
rossanodr <rvdalarosa@hotmail.com>
2023-01-20 19:44:34 -0300

    stripe integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Image from "next/image";
+import type { AppProps } from "next/app";
+import { Container, Header } from "@/styles/pages/app";
+import { globalStyles } from "@/styles/global";
 
+import logoImg from "../assets/logo.svg";
+
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
